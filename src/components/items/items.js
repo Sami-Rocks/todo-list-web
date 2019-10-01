a238d7cc776db08659ce717d6bfcf30ec2aeee6d
Sami <sowusuaboagye@gmail.com>
2019-10-01 17:29:32 +0100

    yets

--- src/components/items/items.js
+++ src/components/items/items.js
@@ -1,33 +1,73 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './items.module.scss';
+import axios from 'axios'
 
+class Item extends Component{
+    
+    state = {
+        tasks: []
+    }
 
-const item = ({tasks}) =>{
-    return(
-        <div className={classes.Item}>
-            <h3>Todo List</h3>
-            <table>
-                <thead>
-                <tr>
-                    <th className={classes.HText}>List</th>
-                    <th>Status</th>
-                    <th>Close</th>
-                </tr>
-                
-                </thead>
-                <tbody>
-                    {tasks.map((task) => (
-                        <tr>
-                            <td className={classes.Text}>{task.title}</td>
-                            <td className={classes.Status}><a href="#">{task.completed}</a></td>
-                            <td className={classes.Remove}><a href="#"><i className="fas fa-trash-alt"></i></a></td>
-                        </tr>
-                    ))}
+    getTasks(){
+        fetch(`https://isktodo.herokuapp.com/todos-api/todos/`)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+              tasks: data
+          })
+        })
+    }
+    
+    
+    componentDidMount() {
+        this.getTasks();
+        
+      }
+    
+      deleteTask(e, value){
+        fetch('https://isktodo.herokuapp.com/todos-api/todos/'+value, { 
+        method: 'DELETE' 
+        }); 
+ 
+    }
+    
+      completion(value){
+        if(value===true){
+            return('Completed');
+        }else{
+            return('pending');
+        }
+    }
+    
+    render(){
+    
+        return(
+            <div className={classes.Item}>
+                <h3>Todo List</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th className={classes.HText}>List</th>
+                        <th>Status</th>
+                        <th>Close</th>
+                    </tr>
+                    
+                    </thead>
+                    <tbody>
+                        {this.state.tasks.map((task) => (
+                            <tr key={task.id}>
+                                <td className={classes.Text}>{task.title}</td>
+                                <td className={classes.Status}><button onClick="">{this.completion(task.completed)}</button></td>
+                                <td className={classes.Remove}><button onClick={(e) =>this.deleteTask(e, task.id)}><i className="fas fa-trash-alt"></i></button></td>
+                            </tr>
+                        ))}
+    
+                    </tbody>
+                </table>
+            </div>
+        );
 
-                </tbody>
-            </table>
-        </div>
-    );
+    }
 }   
 
-export default item;
+export default Item;
--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,26 +1,15 @@
 import React, {Component} from 'react';
 import classes from './list.module.scss';
 import Item from './../items/items';
+import axios from 'axios';
 
 class List extends Component{
 
-    state = {
-        tasks: []
-    }
-
-    componentDidMount() {
-        fetch('https://isktodo.herokuapp.com/todos-api/todos/')
-        .then(res => res.json())
-        .then((data) => {
-          this.setState({ tasks: data })
-        })
-        .catch(console.log)
-      }
 
     render(){
         return(
             <div className={classes.List}>
-               <Item tasks={this.state.tasks} />
+               <Item />
               
             </div>
         );
